117572d4e922f7c0e0797b04225066844af47435
Pugga <38046181+GoodEgUY@users.noreply.github.com>
2023-07-19 00:35:57 +0300

    Add files via upload

---
+++ src/components/Main/Projects/Projects.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./projects.css";
+
+const Projects = () => {
+  return (
+    
+    <div className="projects">
+      
+        <div className="projectItem">
+          <img src="./images/champ.png" alt="" className="projectImage" />
+          <div className="projectText">
+            <p className="projectName">Coming Soon...</p>
+            <p className="projectTitle">Marketing Agency</p>
+          </div>
+        </div>
+      
+    </div>
+    
+  );
+};
+
+export default Projects;
