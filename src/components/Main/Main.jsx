966391928edb97127a6d591c9e4fce2962a716be
GoodEgUY <aezakmi2508@gmamil.com>
2023-07-19 00:40:12 +0300

    dfdf

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,128 +1,37 @@
 import "./main.css";
 import { useState } from "react";
-import Projects from "./Projects/Projects";
 
 function Main() {
-  const [startOpened, setStartOpened] = useState(true);
-
-  const [UxProject, setUxProject] = useState(false);
-  const [UiProject, setUiProject] = useState(false);
-  const openStart = () => {
-    setStartOpened(true);
-    setUxProject(false);
-    setUiProject(false);
-  };
-  const openUxProject = () => {
-    setStartOpened(false);
-    setUxProject(true);
-    setUiProject(false);
-  };
-  const openUiProject = () => {
-    setStartOpened(false);
-    setUxProject(false);
-    setUiProject(true)
-  };
   return (
     <>
       <div className="main">
         <div className="wrapper">
-          <img src="./images/tplinks.png" alt="" className="ava" />
-          <div className="portNavigation">
-            <p
-              className={
-                startOpened
-                  ? "aboutPortfolioNavLink activeAbNL"
-                  : "aboutPortfolioNavLink"
-              }
-              onClick={openStart}
-            >
-              Develop
-            </p>
-            <p
-              className={
-                UiProject
-                  ? "aboutPortfolioNavLink activeAbNL"
-                  : "aboutPortfolioNavLink"
-              }
-              onClick={openUiProject}
-            >
-              Projects
-            </p>
-            <p
-              className={
-                UxProject
-                  ? "aboutPortfolioNavLink activeAbNL"
-                  : "aboutPortfolioNavLink"
-              }
-              onClick={openUxProject}
-            >
-              Other
-            </p>
+          <img src="./images/smoke2.png" alt="" className="smokeImg"/>
+          <img src="./images/smoke1.png" alt="" className="smokeImg2"/>
+          <img src="./images/logo.JPG" alt="" className="ava" />
+
+          <div className="dashbord">
+            <a href="https://forms.gle/niy4LEhAn54ieree6">
+              <div className="button">
+                <p>INSTAGRAM</p>
+                <img src="./images/icon1.png" alt="" className="ico" />
+              </div>
+            </a>
+            <a href="https://github.com/GoodEgUY">
+              <div className="button">
+                <p>TELEGRAM</p>
+                <img src="./images/icon1.png" alt="" className="ico" />
+              </div>
+            </a>
+            <a href="https://www.linkedin.com/in/ilya-pugachev-82b28a238/">
+              <div className="button">
+                <p>441114454822238</p>
+                <img src="./images/icon1.png" alt="" className="ico" />
+              </div>
+            </a>
+
+            
           </div>
-          {startOpened ? (
-            <div className="dashbord">
-              <a href="https://forms.gle/niy4LEhAn54ieree6">
-                <div className="button">
-                  <p>Замовити розробку</p>
-                  <img src="./images/icon1.png" alt="" className="ico" />
-                </div>
-              </a>
-              <a href="https://github.com/GoodEgUY">
-                <div className="button">
-                  <p>Github</p>
-                  <img src="./images/icon1.png" alt="" className="ico" />
-                </div>
-              </a>
-              
-              
-              <a href="https://www.linkedin.com/in/ilya-pugachev-82b28a238/">
-                <div className="button">
-                  <p>LinkedIn</p>
-                  <img src="./images/icon1.png" alt="" className="ico" />
-                </div>
-              </a>
-             
-              <a href="https://drive.google.com/file/d/1WmMyv6tPPNWeufI_P-uoICpJmE6YVA5Y/view?usp=drivesdk">
-                <div className="button">
-                  <p>CV</p>
-                  <img src="./images/Icon5.png" alt="" className="ico" />
-                </div>
-              </a>
-             
-            
-              
-            </div>
-            
-          ) :  null} { UxProject ? (
-            
-            <div className="dashbord">
-              <a href="https://t.me/thepugga_pr">
-                <div className="button">
-                  <p>Telegram</p>
-                  <img src="./images/Icon5.png" alt="" className="ico" />
-                </div>
-              </a>
-              
-              <a href="https://t.me/trapbomber">
-                <div className="button">
-                  <p>Music Channel TG</p>
-                  <img src="./images/icon1.png" alt="" className="ico" />
-                </div>
-              </a>
-              <a href="https://twitter.com/Thepugga1">
-                <div className="button">
-                  <p>Twitter</p>
-                  <img src="./images/Icon5.png" alt="" className="ico" />
-                </div>
-              </a>
-              <a href="https://soundcloud.com/thepugga">
-                <div className="button">
-                  <p>SoundCloud</p>
-                  <img src="./images/Icon5.png" alt="" className="ico" />
-                </div>
-              </a>
-            </div>
-          ) : null } {UiProject ? <Projects/>: null}
         </div>
       </div>
     </>
