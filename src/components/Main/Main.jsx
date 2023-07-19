29b0c32fcc90b68b36c455310783c851fdfff190
Pugga <aezakmi2508@gmail.com>
2023-07-19 16:38:44 +0300

    Aded copy to clipboard and notification

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,14 +1,27 @@
 import "./main.css";
+import { useClipboard } from "use-clipboard-copy";
+import { useState } from "react";
 
+function Main() {  const [ modal, setModal] = useState(false)
+  const clipboard = useClipboard()
+  const copyModal = () => {
+    setModal(true)
+    setTimeout(() => {
+      setModal(false)
+    }, 2000)
+    
 
-function Main() {
+  }
   return (
     <>
       <div className="main">
         <div className="wrapper">
+          <div className={modal ? "notification animNotif" : "notification"}>
+            <p>номер картки успішно скопійовано</p>
+          </div>
           <img src="./images/smoke2.png" alt="" className="smokeImg"/>
           <img src="./images/smoke1.png" alt="" className="smokeImg2"/>
-          <img src="./images/logo.JPG" alt="" className="ava" />
+          <img src="./images/logo.png" alt="" className="ava" />
 
           <div className="dashbord">
             <a href="https://forms.gle/niy4LEhAn54ieree6">
@@ -23,9 +36,12 @@ function Main() {
                 <img src="./images/icon1.png" alt="" className="ico" />
               </div>
             </a>
-            <a href="https://www.linkedin.com/in/ilya-pugachev-82b28a238/">
-              <div className="button">
-                <p>441114454822238</p>
+            
+              <a>
+              <div className="button" onClick={copyModal} onFocus={clipboard.copy}>
+               
+              <input ref={clipboard.target} value={"441114454822238"} readOnly hidden />
+                <p>Сплатити</p>
                 <img src="./images/icon1.png" alt="" className="ico" />
               </div>
             </a>
