e064c4b23083dab138c025c6130857ae99c24a20
Pugga <38046181+GoodEgUY@users.noreply.github.com>
2023-10-23 13:53:13 +0300

    Update Main.jsx

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -18,17 +18,17 @@ function Main() {
         <div className="wrapper">
           <img src="./images/smoke2.png" alt="" className="smokeImg" />
           <img src="./images/smoke1.png" alt="" className="smokeImg2" />
-          <img src="./images/logo.png" alt="" className="ava" />
+          <img src="./images/logo2.png" alt="" className="ava" />
 
           <div className="dashbord">
             
-            <a href="https://instagram.com/amy_shop_kharkiv?igshid=MzRlODBiNWFlZA==">
+            <a href="https://instagram.com/kalyanchik_kh?igshid=MzRlODBiNWFlZA==">
               <div className="button">
                 <p>INSTAGRAM</p>
                 <img src="./images/inst.png" alt="" className="ico" />
               </div>
             </a>
-            <a href="https://t.me/amy_shop_kh">
+            <a href="https://t.me/kalyanchik_kh">
               <div className="button">
                 <p>TELEGRAM</p>
                 
