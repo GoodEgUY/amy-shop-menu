5aff97c9146b859bbbd02ea6bd6e4e5e89c55cbe
Pugga <38046181+GoodEgUY@users.noreply.github.com>
2024-07-11 03:12:08 +0300

    Update Main.jsx

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -7,7 +7,7 @@ function Main() {
   const clipboard = useClipboard();
   const copyModal = () => {
     setModal(true);
-    clipboard.copy('4441111154790897')
+    clipboard.copy('4441111158170153')
     setTimeout(() => {
       setModal(false);
     }, 6000);
@@ -37,7 +37,7 @@ function Main() {
               </div>
             </a>
 
-            <a href="https://send.monobank.ua/8a1Sx3Pq6Q">
+            <a href="https://send.monobank.ua/922f4W5hPw">
               <div className="button">
                 <p>MONOBANK</p>
 
